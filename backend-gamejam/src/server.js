f23da18a13dbf7779202f9614d3eb9fb9bd5527b
MigueReyRo <90188544+MigueReyRo@users.noreply.github.com>
2024-04-08 21:48:10 +0200

    Server TaleWeaver

---
+++ backend-gamejam/src/server.js
@@ -0,0 +1,59 @@
+import express from 'express';
+import cors from 'cors'; // Importa cors
+import fetch from 'node-fetch';
+import bodyParser from 'body-parser';
+import dotenv from 'dotenv';
+
+
+dotenv.config();
+
+const app = express();
+
+
+// Usa cors para permitir todas las solicitudes cruzadas
+// Para un ambiente de producción, querrás configurar esto para permitir solo ciertos orígenes
+const corsOptions = {
+    origin: process.env.ALLOWED_ORIGIN || 'http://localhost:3001', // Origen por defecto para desarrollo
+};
+
+app.use(cors(corsOptions));
+
+app.use(bodyParser.json());
+
+app.post('/nfts', async (req, res) => {
+    const { address } = req.body;
+    const apiKey = process.env.ARK_API_KEY;
+    const url = `https://api.arkproject.dev/v1/owners/${address}/tokens?contract_address=0x0169e971d146ccf8f5e88f2b12e2e6099663fb56e42573479f2aee93309982f8`;
+    
+
+    try {
+        const response = await fetch(url, {
+            method: 'GET',
+            headers: { 'x-api-key': apiKey },
+        });
+
+        if (!response.ok) throw new Error(`Error en la respuesta de la API: ${response.statusText}`);
+
+        const data = await response.json();
+        // Si la API devuelve una respuesta exitosa pero sin NFTs, puedes manejar ese caso aquí
+        if (data.result.length === 0) {
+            console.log("No NFTs found for this address:", address);
+            // Envía una respuesta exitosa indicando que no se encontraron NFTs
+            return res.status(200).json({ message: "No NFTs found for this address" });
+        }
+
+        // Si se encuentran NFTs, devuelve esos datos
+        console.log(`Sending NFT data to client for address: ${address}`);
+        res.json(data);
+    } catch (error) {
+        console.error('Error fetching NFTs:', error);
+        console.log("Address attempted:", address);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`Server listening at http://localhost:${port}`);
+});
